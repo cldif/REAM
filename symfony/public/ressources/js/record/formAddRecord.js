f057ef6457186f74ca6f1856a91b1e0987e0c922
Clément Dif <clement.dif@etu.isima.fr>
2020-01-02 16:53:10 +0100

    :lipstick: Created the page for adding a record.

---
+++ symfony/public/ressources/js/record/formAddRecord.js
@@ -0,0 +1,7 @@
+$("select").removeClass("form-control");
+$("select").addClass("mdb-select md-form");
+
+// Material Select Initialization
+$(document).ready(function() {
+  $(".mdb-select").materialSelect();
+});
