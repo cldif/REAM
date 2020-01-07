15451aa8d9f8b8f1b499bd009346d91c30b35fa7
sybessonne <sylvain.bessonneau@etu.isima.fr>
2020-01-07 13:30:16 +0100

    bug correction

--- symfony/src/Form/LocalType.php
+++ symfony/src/Form/LocalType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
