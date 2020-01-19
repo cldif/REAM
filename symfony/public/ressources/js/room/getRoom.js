03e9f23b7ee0ea33c588af0ef0decfdb849ceb6f
Clément Dif <clement.dif@etu.isima.fr>
2020-01-19 01:11:00 +0100

    :children_crossing: Allows the user to add a template relative to a single room.

--- symfony/public/ressources/js/room/getRoom.js
+++ symfony/public/ressources/js/room/getRoom.js
@@ -1,7 +1,10 @@
 let deleteRoomUrl = $("#js-passthrough-delete-room-url").text();
-let websiteIndex = $("#js-passthrough-index-url").text();
+let websiteIndexUrl = $("#js-passthrough-index-url").text();
+let addDocumentUrl = $("#js-passthrough-add-document-url").text();
 
-function makeAjaxRequest() {
+/* Ajax Requests definitions */
+
+function deleteRoomAR() {
   return new Promise((resolve, reject) => {
     $.ajax({
       url: deleteRoomUrl,
@@ -16,16 +19,57 @@ function makeAjaxRequest() {
   });
 }
 
+function addDocumentAR(url, file, name) {
+  let formData = new FormData();
+  formData.append("document", file);
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: "POST",
+      headers: { documentName: name },
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(result) {
+        resolve(result);
+      },
+      error: function(result) {
+        reject(result);
+      }
+    });
+  });
+}
+
+/* Ajax Requests bindings */
+
 $("#delete-room-button").on("click", function() {
-  makeAjaxRequest()
+  deleteRoomAR()
     .then(data => {
-      console.log("%c Deleted successfully", "color:green;");
+      console.log("%c Room deleted successfully", "color:green;");
       console.log(data);
-      $(location).attr("href", websiteIndex);
+      $(location).attr("href", websiteIndexUrl);
     })
     .catch(error => {
-      console.log("%c Error during deletion", "color:red;");
+      console.log("%c Error during room deletion", "color:red;");
       console.log(error);
-      $(location).attr("href", websiteIndex);
+      $(location).attr("href", websiteIndexUrl);
+    });
+});
+
+$("#add-document-button").on("click", function() {
+  let file = document.getElementById("file-input").files[0];
+  let fileName = $("#file-name").val();
+
+  addDocumentAR(addDocumentUrl, file, fileName)
+    .then(data => {
+      console.log("%c Document added successfully", "color:green;");
+      console.log(data);
+      location.reload(true);
+    })
+    .catch(error => {
+      console.log("%c Error when adding a document.", "color:red;");
+      console.log(error);
+      location.reload(true);
     });
 });
