d2eb8901aa9d343c592642e8191148f66a95ec49
sybessonne <sylvain.bessonneau@etu.isima.fr>
2020-02-02 18:41:21 +0100

    fix bug record

--- symfony/src/Controller/RecordController.php
+++ symfony/src/Controller/RecordController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Response;
 
 use App\Entity\Record;
 use App\Entity\Tenant;
